1bc1e9b375e53353b9902ae26b4a2f99be91e5d8
Melody Plan <melodyplan@gmail.com>
2017-04-07 11:18:55 -0700

    trying to figure out unique word count

--- app.js
+++ app.js
@@ -3,15 +3,34 @@
 //need to figure out a function for unique word count...
 
 var textAnalyzer = function(words) {
-    //var uniqueWordCount = ?? - figure this out tomorrow? later?
     var wordCount = words.split(" ").length;
     var analyzedWords = words.split(" ");
     var avgWords = 0;
     for (var i = 0; i < wordCount.length; i++) {
         avgWords += analyzedWords[i].length;
     }
-    var avgLength = avgWord / wordCount;
+    var avgLength = avgWord / wordCount; 
 };
+
+//creating a new function for uniqueWordCount...
+//i know i need to make a variable called userText for an input of whatever they put in...
+
+var userText = [];
+
+/*should probably create a function that translates the user's random words from English
+to actual separate string words that the computer understands... split?*/
+
+function translateUserText(userBabble) {
+    return userBabble.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+};
+
+//now what. 
+
+function calculate(uniqueWordCount) {
+    for (var i = 0; i < result.length; i++) {
+        if (userText())
+    }
+}
 
 //ugh how to connect this to html and not just console.log it though? 
 
